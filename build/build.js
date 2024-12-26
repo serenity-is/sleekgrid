5a3b6fbc5d473bdeed3fa6f75ea96a2e9f63d6d2
volkanceylan <volkanceylan@gmail.com>
2024-12-26 13:13:14 +0300

    Fix writeIfChanged plugin

--- build/build.js
+++ build/build.js
@@ -33,22 +33,23 @@ export function globalExternals(filter, externals) {
     };
 }
 
+
 export function writeIfChanged() {
     return {
         name: "write-if-changed",
         setup(build) {
-            const write = build.initialOptions.write;
-            build.initialOptions.write = false;
             build.onEnd(result => {
-                if (write === undefined || write) {
-                    result.outputFiles?.forEach(file => {
-                        if (!existsSync(file.path) ||
-                            readFileSync(file.path, 'utf8') !== file.text) {
-                            mkdirSync(dirname(file.path), { recursive: true });
-                            writeFileSync(file.path, file.text);
-                        }
-                    });
-                }
+                result.outputFiles?.forEach(file => {
+                    if (existsSync(file.path)) {
+                        const old = readFileSync(file.path);
+                        if (old.equals(file.contents))
+                            return;
+                    }
+                    else {
+                        mkdirSync(dirname(file.path), { recursive: true });
+                    }
+                    writeFileSync(file.path, file.text);
+                });
             });
         }
     };
